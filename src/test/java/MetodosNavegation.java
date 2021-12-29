eabeb258783f2653f3f24ac6db9910650aaa94ec
Cavalcante, Caio <caio.cavalcante@accenture.com>
2021-12-29 18:03:49 -0300

    Funcionalidades de nevegação

---
+++ src/test/java/MetodosNavegation.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MetodosNavegation {
+	private WebDriver driver;
+	private WebElement file;
+	
+	
+	// Codigo para fazer upload
+	@Test
+	public void MetodoNavigatio() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "chromedriver/chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("Mano Caio");
+		driver.findElement(By.name("btnK")).submit();
+		driver.navigate().to("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("testestes");
+		driver.navigate().refresh();
+		Thread.sleep(2000);
+		driver.navigate().back();// para trás seta
+		Thread.sleep(2000);
+		driver.navigate().forward();//pra frente seta	
+		
+		
+		
+		
+		
+}
+}
